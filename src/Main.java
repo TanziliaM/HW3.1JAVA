dfa7e44a778e4eca775a4edc4b9fae1f82f2207b
Tanzilia Musatova <musatova.tanzilia@gmail.com>
2021-05-19 15:22:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       double amount = 3455.34;
+       int mile1 = 20;
+       int bonus = (int) (amount / mile1);
+    System.out.println(bonus);
+    
+}
+}
